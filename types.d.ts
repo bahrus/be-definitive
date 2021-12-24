7ee80dc33c6819de3db9ca03387da85c0b7a3212
Bruce Anderson <anderson.bruce.b@gmail.com>
2021-12-24 13:06:34 -0500

    0.0.19

--- types.d.ts
+++ types.d.ts
@@ -1,5 +1,10 @@
-export interface BeDefinitiveProps{
+import {DefineArgs} from 'xtal-element/src/types';
+export interface BeDefinitiveVirtualProps extends DefineArgs{
 
+}
+
+export interface BeDefinitiveProps extends BeDefinitiveVirtualProps{
+    proxy: Element & BeDefinitiveVirtualProps;
 }
 
 export interface BeDefinitiveActions{
