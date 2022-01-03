aba82b4a95d85019f088ad0c898e0a494cd6342d
Bruce Anderson <anderson.bruce.b@gmail.com>
2022-01-03 15:58:39 -0500

    0.0.24

--- types.d.ts
+++ types.d.ts
@@ -2,6 +2,11 @@ import {DefineArgs} from 'xtal-element/src/types';
 import { PropSettings } from '../trans-render/lib/types';
 export interface BeDefinitiveVirtualProps<Props = any, Actions = Props> extends DefineArgs<Props, Actions>{
     scriptRef?: string;
+
+    /**
+     * Used for providing hints to server side processing using the HTML what queries should be observed if using HTMLRewriter.
+     */
+    keyQueries: string[];
 }
 
 export interface BeDefinitiveProps<Props = any, Actions = Props> extends BeDefinitiveVirtualProps<Props, Actions>{
