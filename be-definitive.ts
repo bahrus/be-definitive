584115614000d70222484b2f215a59ec0e46e567
Bruce Anderson <anderson.bruce.b@gmail.com>
2022-02-19 11:15:41 -0500

    0.0.36

--- be-definitive.ts
+++ be-definitive.ts
@@ -1,6 +1,5 @@
 import {define, BeDecoratedProps} from 'be-decorated/be-decorated.js';
 import {BeDefinitiveProps, BeDefinitiveActions, BeDefinitiveVirtualProps} from './types';
-//import {TemplMgmtActions, TemplMgmtProps, tm} from 'trans-render/lib/mixins/TemplMgmtWithPEST.js';
 import {Action, TemplMgmt, TemplMgmtActions, TemplMgmtProps, beTransformed} from 'trans-render/lib/mixins/TemplMgmt.js';
 import {register} from 'be-hive/register.js';
 
