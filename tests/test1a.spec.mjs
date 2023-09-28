ffc55e71c6976e864da6a236b3a0d7b9f0c438e0
Bruce B. Anderson <anderson.bruce.b@gmail.com>
2023-09-28 17:18:06 -0400

    0.0.109

--- tests/test1a.spec.mjs
+++ tests/test1a.spec.mjs
@@ -1,5 +1,5 @@
 import { test, expect } from '@playwright/test';
-test('test1', async ({ page }) => {
+test('test1a', async ({ page }) => {
     await page.goto('./tests/ExampleIa.html');
     // wait for 1 second
     await page.waitForTimeout(12000);
--- tests/test4d.spec.mjs
+++ tests/test4d.spec.mjs
@@ -1,5 +1,5 @@
 import { test, expect } from '@playwright/test';
-test('test1', async ({ page }) => {
+test('test4d', async ({ page }) => {
     await page.goto('./tests/ExampleIVd.html');
     // wait for 1 second
     await page.waitForTimeout(12000);
